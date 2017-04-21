81ae873baf2e73b3a1d50e21a7391796ac0a17e8
LiuChengCheng-Jack <LiuChengCheng-Jack@outlook.com>
2017-04-21 16:36:32 +0800

    The first edition of get lable from LatLng

---
+++ div.cpp
@@ -0,0 +1,20 @@
+#include "div.h"
+#include "lable.h"
+#include<math.h>
+#include<iostream>
+using namespace std;
+int getLabelForPoint(double lat,double lng,int * data,int bmpWidth,int bmpHeight) //lat代,纬度，lng代表经度,data代表图片的二值数组
+{
+     double startLat=31.1500000,endLat=31.3700000;
+     double startLng=121.3100000,endLng=121.8400000;
+     int * lable=Lable(data,bmpWidth,bmpHeight);
+ 	 int left=7,right=0,top=3,bottom=13;//要减去截图的那些像素点
+	 int i=0,j=0;
+     double pointLat=(endLat-startLat)*10000.0l/((bmpHeight-top-bottom)*1.0l);
+     double pointLng=(endLng-startLng)*10000.0l/((bmpWidth-left-right)*1.0l);//扩大10000倍以减少误差
+	 
+	 i=floor((lng*10000.0l-startLng*10000.0l)/pointLng)+left;
+	 j=floor((lat*10000.0l-startLat*10000.0l)/pointLat)+top;
+
+     return lable[j*bmpWidth+i];
+}
